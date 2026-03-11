32cc12803a3186fdf194d79f40f4ed9b3869d400
Thapakorn Muangchan <thapakon2546@gmail.com>
2026-03-11 14:57:24 +0700

    chore: Prisma5

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
 })
 export class AppModule {}
